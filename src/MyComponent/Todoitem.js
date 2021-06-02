7391337e46d9ac31fe6acd24ba3fe85e81c69633
chitra <chitraraj94@gmail.com>
2021-06-02 21:49:55 +0530

    First one

--- src/MyComponent/Todoitem.js
+++ src/MyComponent/Todoitem.js
@@ -3,7 +3,8 @@ import React from 'react'
 export const Todoitem = () => {
     return (
         <div className="">
-            todo works
+            todo  the work 
         </div>
     )
 }
+   
